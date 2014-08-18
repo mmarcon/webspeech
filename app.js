beec9935291ae87990e4d8aed43e2c6c16897779
Massimiliano Marcon <massimiliano.marcon@here.com>
2014-08-18 13:55:19 +0200

    Port settings

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var static = require('node-static');
 
 var file = new(static.Server)('./dist');
 
+var PORT = process.env.PORT || 1337;
+
+console.log('PORT = ' + PORT);
+
 require('http').createServer(function (request, response) {
     file.serve(request, response);
-}).listen(process.env.VMC_APP_PORT || 1337);
+}).listen(PORT);
