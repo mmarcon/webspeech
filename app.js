3c3abab4e2f2ad853d8239d18df5e4f37d9b3135
Massimiliano Marcon <me@marcon.me>
2014-08-18 08:11:25 +0200

    Change port

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ var file = new(static.Server)('./dist');
 
 require('http').createServer(function (request, response) {
     file.serve(request, response);
-}).listen(process.env.VMC_APP_PORT || 1337);
+}).listen(process.env.PORT || process.env.VCAP_APP_PORT || 3000);
